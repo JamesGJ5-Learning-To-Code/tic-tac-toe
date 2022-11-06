b9328f13603a0fcb739b7527e63c269773ba7f3e
James Graca-Jones <jamesgracajones@gmail.com>
2022-11-06 11:59:53 +0000

    Update plans

--- script.js
+++ script.js
@@ -23,17 +23,12 @@ const gameboard = (() => {
 // DONE Creates two Players
 // Upon receiving the class of the clicked div from displayController:
 // NOTE: the below will be enabled through methods belonging to gameboard
-// 1. Checks that the index of Gameboard equal to Number(class) is not already 
-// occupied.
-// 2. If not:
-// -- a) Fills the index with the relevant marker
-// -- b) Does a win-check (see README.md). If someone wins:
-// ---- i) Make displayController announce a winner (e.g. by alert, can just alert 
+// 1) Fills the index with the relevant marker
+// 2) Does a win-check (see README.md). If someone wins:
+// -- a) Make displayController announce a winner (e.g. by alert, can just alert 
 //      the marker for now)
-// ---- ii) Make displayController disable the event listeners
-//  -- c) Returns the marker to displayController
-// Else:
-// -- Returns undefined to displayController (can simply use 'return;')
+// -- b) Make displayController disable the event listeners
+//  3) Returns the marker to displayController
 
 const game = (() => {
     const player1 = Player('X');
@@ -45,10 +40,16 @@ const game = (() => {
 // This should have a method that takes the container grid and adds event listeners 
 // to it that, ultimately:
 // 1. Check for a click (in displayController)
-// 2. Supplies the class of the div to Game
-// 3. Gets back from the Game the marker of the player whose turn it is if 
-// anything should be played, else undefined
+// 2. Check if the div is filled already. If not:
+//  a) Supplies the class of the div to Game
+//  b) Gets back from the Game the marker of the player whose turn it is
 
 const displayController = (() => {
     const gridDiv = document.querySelector('.grid');
+    // const cellDivList = gridDiv.childNodes;
+    // cellDivList.forEach((cellDiv) => {
+    //     cellDiv.addEventListener('click', () => {
+            
+    //     });
+    // });
 })();
